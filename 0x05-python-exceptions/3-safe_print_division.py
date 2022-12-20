69ae7f452d53249d02e8925b68238b9911661524
Rachelozuem <ozuemrachel@gmail.com>
2022-12-20 15:09:03 -0800

    difference between errors and exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
-     try:
-         a = a / b
-     except ZeroDivisionError:
-         a = None
-     finally:
-          print("Inside result:", "{}".format(a))
-      return a
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
